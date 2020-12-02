1228f7326d2f1adb2f74f69024e89a612629d292
jhnim <jhnim1313@gmail.com>
2020-12-03 08:07:56 +0900

    .

--- src/model/MemberDTO.java
+++ src/model/MemberDTO.java
@@ -40,9 +40,9 @@ public class MemberDTO {
 	}
 	
 	//getter/setter
-	public String getId() {
-		return id;
-	}
+	
+	public String getId() { return id; }
+	 
 
 	public void setId(String id) {
 		this.id = id;
